4b14747fcec322a5f736d534bff36fd327583f81
faithcredit <honest.dedicate@gmail.com>
2023-07-16 20:03:21 +0900

    Inserting link tracking code in the page body using plugin filters

--- wp-content/plugins/ch2-page-header-output/ch2-page-header-output.php
+++ wp-content/plugins/ch2-page-header-output/ch2-page-header-output.php
@@ -1,8 +1,8 @@
 <?php
 /*
-  Plugin Name: Chapter 2 - Page Header Output
+  Plugin Name: Chapter 2 - Page Header Output V2
   Plugin URI: 
-  Description: Companion to recipe 'Adding output content to page headers using plugin actions'
+  Description: Companion to recipe 'Inserting link statistics tracking code in page body using plugin filters'
   Author: ylefebvre
   Version: 1.0
   Author URI: http://ylefebvre.ca/
@@ -21,9 +21,34 @@ function ch2pho_page_header_output() { ?>
 		m.parentNode.insertBefore(a,m)})(window,document,'script',
 		'https://www.google-analytics.com/analytics.js','ga');
 
-
 		ga( 'create', 'UA-0000000-0', 'auto' );
 		ga( 'send', 'pageview' );
 	</script>
+
+<?php }
+
+add_filter( 'the_content', 'ch2lfa_link_filter_analytics' );
+
+function ch2lfa_link_filter_analytics ( $the_content ) {
+	$new_content = str_replace( 'href', 'onClick="recordOutboundLink(this);return false;" href', $the_content );
+
+	return $new_content;
+}
+
+add_action( 'wp_footer', 'ch2lfa_footer_analytics_code' );
+
+function ch2lfa_footer_analytics_code() { ?>
+    
+<script type="text/javascript">
+  function recordOutboundLink( link ) {
+	ga('send', 'event', 'Outbound Links', 'Click',
+		link.href, {
+			'transport': 'beacon',
+			'hitCallback': function() { 
+				document.location = link.href; 
+			}
+		} );
+	}
+</script>
 
 <?php }
